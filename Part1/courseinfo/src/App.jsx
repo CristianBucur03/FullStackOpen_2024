5b49f3c6309f8fe98c6ebc0fcea3aa4750e93734
Cristian Bucur <cristian.bucur@dedalus.eu>
2024-08-29 15:58:56 +0100

    ex.1.4

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,20 +1,33 @@
 /* eslint-disable react/prop-types */
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
-  // Refactor the code so that it consists of three new components: Header, Content, and Total.
+  // const part1 = {
+  //   name: "Fundamentals of React",
+  //   exercises: 10,
+  // };
+  // const part2 = {
+  //   name: "Using props to pass data",
+  //   exercises: 7,
+  // };
+  // const part3 = {
+  //   name: "State of a component",
+  //   exercises: 14,
+  // };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+  //
 
   const Header = (props) => {
     return (
@@ -33,22 +46,22 @@ const App = () => {
       </div>
     );
   };
-  const Content = () => {
+  const Content = (parts) => {
     return (
       <div>
-        <Part part={part1.name} exercises={part1.exercises} />
-        <Part part={part2.name} exercises={part2.exercises} />
-        <Part part={part3.name} exercises={part3.exercises} />
+        {parts.map((each) => (
+          <Part key={each.index} part={each.name} exercises={each.exercises} />
+        ))}
       </div>
     );
   };
 
-  const Total = () => {
+  const Total = (parts) => {
     return (
       <div>
         <p>
           Number of exercises{" "}
-          {part1.exercises + part2.exercises + part3.exercises}
+          {parts[0].exercises + parts[1].exercises + parts[2].exercises}
         </p>
       </div>
     );
@@ -57,8 +70,8 @@ const App = () => {
   return (
     <div>
       <Header title={course} />
-      <Content />
-      <Total />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
