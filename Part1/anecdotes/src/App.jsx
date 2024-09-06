3685f718cc9bf05a7d59f0983fcaeefb04b53cf1
Cristian Bucur <cristian.bucur@dedalus.eu>
2024-09-06 13:40:23 +0100

    ex.1.13

--- Part1/anecdotes/src/App.jsx
+++ Part1/anecdotes/src/App.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 
 function App() {
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(7).fill(0));
   const selectAnecdote = () => {
     setSelected(Math.floor(Math.random() * 7));
   };
@@ -15,10 +17,23 @@ function App() {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
+
+  //create a voting system for each anecdote and display it
+
+  const vote = (selected) => {
+    const changePoints = [...points];
+    changePoints[selected] += 1;
+    setPoints(changePoints);
+    console.log(points);
+  };
+
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
+      <div>
+        {anecdotes[selected]} has {points[selected]}points
+      </div>
       <button onClick={() => selectAnecdote()}>Next anecdote</button>
+      <button onClick={() => vote(selected)}>Vote</button>
     </div>
   );
 }
