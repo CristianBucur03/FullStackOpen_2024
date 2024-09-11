455afd75b8aaaa8f3faafb96641197ed553a524f
Cristian Bucur <cristian.bucur@dedalus.eu>
2024-09-11 14:33:52 +0100

    2.9

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: "Mikey Mike", number: "09090909" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filteredList, setFilteredList] = useState([]);
   const [newPerson, setNewPerson] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchInput, setSearchInput] = useState("");
 
   const handleNameInputChange = (event) => {
     setNewPerson(event.target.value);
@@ -13,17 +18,32 @@ function App() {
   const handleNumberInputChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+    defineFilteredList(searchInput);
+  };
 
   const newName = {
     name: newPerson,
     number: newNumber,
+    id: persons.length + 1,
   };
+
   const existingName = persons.filter((each) => each.name === newPerson);
+
+  const defineFilteredList = (searchInput) => {
+    const newFilteredList = persons.filter((each) =>
+      each.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    newFilteredList.length > 0;
+    setFilteredList(newFilteredList);
+  };
 
   const addNewPerson = (event) => {
     event.preventDefault();
 
     const newList = persons.concat(newName);
+
     existingName.length > 0
       ? alert(`${newPerson} is already in the list`)
       : newNumber !== ""
@@ -46,14 +66,25 @@ function App() {
           <button type="submit">Add person</button>
         </div>
       </form>
+      <form>
+        <div>
+          Search person : <input onChange={handleSearchInputChange} />
+        </div>
+      </form>
       <div>
         <h3>Numbers:</h3>
       </div>
-      {persons.map((each, i) => (
-        <p key={i}>
-          {each.name} {each.number}
-        </p>
-      ))}
+      {searchInput !== ""
+        ? filteredList.map((each, i) => (
+            <p key={i}>
+              {each.name} {each.number}
+            </p>
+          ))
+        : persons.map((each, i) => (
+            <p key={i}>
+              {each.name} {each.number}
+            </p>
+          ))}
     </>
   );
 }
